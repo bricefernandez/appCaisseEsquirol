16b42914c10fce04432e78d7272f8de2d66ea1bc
Brice Fernandez <brice.fernandez@crossknowledge.com>
2017-10-22 14:56:01 +0200

    All DB structure with relations

--- backend/models/category.js
+++ backend/models/category.js
@@ -3,7 +3,6 @@ module.exports = function(sequelize, DataTypes) {
   var Category = sequelize.define('Category', {
     name: DataTypes.STRING,
     level: DataTypes.INTEGER,
-    image: DataTypes.STRING,
     parent: DataTypes.INTEGER
   }, {
     classMethods: {
--- backend/models/products.js
+++ backend/models/products.js
@@ -6,6 +6,7 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods: {
       associate: function(models) {
+        Products.hasMany(models.SaleProduct)
       }
     }
   });
