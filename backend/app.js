6c38dc42201a9e599092b3ec55be1c389f60875c
francois tiome <ntm.fdp.com>
2017-10-25 21:12:59 +0200

    allow origin marche sans ta merde d'addon

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+console.log("coucou")
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
@@ -24,20 +26,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.all('/*', function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        //res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+        res.header("Access-Control-Allow-Methods", "GET, POST", "PUT");
+        next();
+
+    });
+
 app.use('/', routes);
 app.use('/category', category);
 app.use('/product', product);
 app.use('/capacity', capacity);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE');
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+
+
 
 // error handler
 // no stacktraces leaked to user unless in development environment
