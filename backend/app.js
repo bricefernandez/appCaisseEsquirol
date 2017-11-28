5e167c4b0dadf4a53d182024f1822be3957ab994
bricefernandez <brice.ferna@gmail.com>
2017-11-28 21:55:15 +0100

    Adding delete rules access

--- backend/app.js
+++ backend/app.js
@@ -27,9 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.all('/*', function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
-        //res.header("Access-Control-Allow-Headers", "X-Requested-With");
         res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
-        res.header("Access-Control-Allow-Methods", "GET, POST", "PUT");
+        res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
         next();
 
     });
