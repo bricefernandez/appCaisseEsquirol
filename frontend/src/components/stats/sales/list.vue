aebc210c9b352e5b8fb058a8df7d6e40030e5341
bricefernandez <brice.ferna@gmail.com>
2017-12-18 23:47:59 +0100

    statspage

--- frontend/src/components/stats/sales/list.vue
+++ frontend/src/components/stats/sales/list.vue
@@ -3,23 +3,36 @@
         <el-row>
             <el-col :span="21" :offset="3">
                 <h1>Toutes les ventes</h1>
-                <div v-for="sale in sales">
+                <el-row class="HeaderRow">
+                  <el-col :span="4">
+                    N°
+                  </el-col>
+                  <el-col :span="4">
+                    Date
+                  </el-col>
+                  <el-col :span="4">
+                    Total
+                  </el-col>
+                  <el-col :span="12">
+                    Liste des produits
+                  </el-col>
+                </el-row>
+                <el-row class="SecondRow" v-for="sale in sales">
                     <el-col :span="4">
-                        Vente N°{{ sale.id }}
+                        {{ sale.id }}
                     </el-col>
                     <el-col :span="4">
-                        Date: {{ sale.date }}
+                       {{ sale.date }}
                     </el-col>
-                    <el-col :span="4">cd
-                        Total: {{ sale.totalPrice }}
+                    <el-col :span="4">
+                       {{ sale.totalPrice }} €
                     </el-col>
                     <el-col :span="12">
                         <div v-for="product in sale.SaleProducts">
-                            <div>Nom produit : {{ product.Product.name }}</div>
-                            <div>Quantité : {{ product.quantity }}</div>
+                            <div>{{ product.Product.name }} x{{ product.quantity }}</div>
                         </div>
                     </el-col>
-                </div>
+                </el-row>
             </el-col>
         </el-row>
     </div>
@@ -53,6 +66,16 @@
   }
 </script>
 
-<style>
+<style scoped>
+
+  .HeaderRow {
+    padding: 15px;
+    background: rgba(211, 211, 211, 0.58);
+    font-weight: bold;
+  }
+
+  .SecondRow {
+    padding: 10px;
+  }
 
 </style>
