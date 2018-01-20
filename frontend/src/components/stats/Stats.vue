364d8fa6e0f2e9c37f65a66b6152b325d7722419
Brice Fernandez <brice.fernandez@crossknowledge.com>
2018-01-20 10:51:44 +0100

    removing left menu stats

--- frontend/src/components/stats/Stats.vue
+++ frontend/src/components/stats/Stats.vue
@@ -1,20 +1,6 @@
 <template>
     <div class="stats">
-        <el-col :span="4">
-            <h5>Menu statistiques</h5>
-            <el-menu
-                    :router="true"
-                    :default-active="$route.path"
-                    class="el-menu-vertical-demo"
-                    @open="handleOpen"
-                    @close="handleClose"
-                    background-color="#FFF"
-                    default-openeds="[1]">
-                <el-menu-item index="/stats" route="/stats"><i class="el-icon-information"></i> Liste des ventes</el-menu-item>
-            </el-menu>
-        </el-col>
-
-        <el-col :span="18">
+        <el-col :span="24">
             <router-view></router-view>
         </el-col>
     </div>
